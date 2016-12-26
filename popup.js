b8aeff03996934a1b47d4f6d76ff1888bd359af9
CVasquezG <camilo11295@gmail.com>
2016-12-26 15:39:12 -0800

    added files

---
+++ popup.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    alert("HELLO");
+    window.location= 'https://www.youtube.com/feed/subscriptions';
+  });
